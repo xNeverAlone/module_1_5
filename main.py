ddcf495271874dabb9090d970faf59f9e03c7bd1
xNeverAlone <barretsv@gmail.com>
2024-11-23 18:56:44 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# immutable_var = (1, 2, 3, True, 'String')
+# print(immutable_var)
+# immutable_var[4] = 200
+
+mutable_list = [1, 2, 3, 4, 5]
+print(mutable_list)
+mutable_list[0:3] = 200, 10
+print(mutable_list)
